c807dc98388d5fbb9242e37ee3b54fa10082791a
Erfan Ajorlu <erfanajoglu098@gmail.com>
2024-01-07 10:41:26 +0330

    connect to mongodb

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-const express = require('express');
-const expressLayouts = require('express-ejs-layouts');
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
 
 const app = express();
 
+//DB confing
+const db = require("./config/keys").MongoURI;
+
+//connect to mongo
+mongoose.connect(db)
+.then(()=>console.log('mongo connected'))
+.catch(err => console.log(err));
+
 //EJS
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(expressLayouts);
-app.set('view engine' , 'ejs')
+app.set("view engine", "ejs");
 
 //Routes
-app.use('/' , require('./routes/index'))
-app.use('/users' , require('./routes/users'))
+app.use("/", require("./routes/index"));
+app.use("/users", require("./routes/users"));
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(PORT , console.log(`Server started on ${PORT} ...`))
+app.listen(PORT, console.log(`Server started on ${PORT} ...`));
---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  pasword: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User' , UserSchema)
+
+module.exports = User;
