c717291185058dcee0bb6d32fed9fce5286391d3
Erfan Ajorlu <erfanajoglu098@gmail.com>
2024-01-08 16:08:12 +0330

    register handle

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ app.use(express.static("public"));
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
+
+//Bodyparser
+app.use(express.urlencoded({extended : false}));
+
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,36 @@ router.get('/register' , (req , res) => {
     res.render('register');
 })
 
+// Register Handle
+router.post("/register" , (req,res)=>{
+    const {name , email , password , password2} = req.body;
+    let errors = [];
+
+    //Check required fields
+    if(!name || !email || !password || !password2){
+        errors.push({msg : 'Please fill in all fields'});
+    }
+
+    //check password matches
+    if (password !== password2){
+        errors.push({msg : 'Passwords do not match'});
+    }
+
+    //check pass length
+    if(password.length < 6){
+        errors.push({msg: "Password must be at least 6 characters"});
+    }
+
+    if(errors.length > 0){
+        res.render('register' , {
+            errors ,
+            name ,
+            email ,
+            password ,
+            password2
+        })
+    }else{
+        res.send('pass')
+    }
+})
 module.exports = router;
