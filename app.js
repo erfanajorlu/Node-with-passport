8b4d8ef4f1abec57624abf4856531a244846eae1
Erfan Ajorlu <erfanajoglu098@gmail.com>
2024-01-09 10:23:37 +0330

    add cookie-seisson and flash-connect to project

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const flash = require("connect-flash");
+const session = require("cookie-session");
 
 const app = express();
 
@@ -8,18 +10,38 @@ const app = express();
 const db = require("./config/keys").MongoURI;
 
 //connect to mongo
-mongoose.connect(db)
-.then(()=>console.log('mongo connected'))
-.catch(err => console.log(err));
+mongoose
+  .connect(db)
+  .then(() => console.log("mongo connected"))
+  .catch((err) => console.log(err));
 
 //EJS
 app.use(express.static("public"));
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
-
 //Bodyparser
-app.use(express.urlencoded({extended : false}));
+app.use(express.urlencoded({ extended: true }));
+
+//Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+// Connect flash
+app.use(flash());
+
+//Global variables
+app.use((req , res , next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
 
 //Routes
 app.use("/", require("./routes/index"));
